0eb1412868efcc5e7e3acc0fdae38e00c6652f2a
snagat <thuglifevideo7@gmail.comcgit>
2023-07-11 02:58:42 +0100

    tomorrow

--- composetest/srcs/requirements/mariadb/tools/script.sh
+++ composetest/srcs/requirements/mariadb/tools/script.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
+
 if  [ ! -d /var/lib/mysql/mariadb ]; then
-    service mysql start
+    echo "SURE"
+    service mariadb start
 
-    mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-    
-    mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PW';"
-    
-   
-    mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%';"
-
-    mysql -e "FLUSH PRIVILEGES;"
-
-    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PW';"
-
-    mysql -u root -p$MYSQL_ROOT_PW -e "FLUSH PRIVILEGES;"
-   
-    kill $(cat /var/run/mysqld/mysqld.pid)
+mysql << EOF
+CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PW';
+GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%';
+FLUSH PRIVILEGES;
+EOF
 fi
-
+echo "exec"
 exec "$@"
